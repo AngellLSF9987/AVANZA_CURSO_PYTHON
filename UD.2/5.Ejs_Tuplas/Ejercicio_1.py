909f63497ede06f1d1041da2e9ab86da1b397704
Angel Luis <alsaorin9987@gmail.com>
2024-09-18 19:47:57 +0200

    Estructura Nueva Directorio y ficheros Ejercicios de Tuplas

---
+++ UD.2/5.Ejs_Tuplas/Ejercicio_1.py
@@ -0,0 +1,21 @@
+"""
+        >>>>>>>    Ejercicios de Tuplas   <<<<<<
+                                                             """
+print (f">>>>>>>    Ejercicios de Tuplas   <<<<<<")                                                            
+print (f"\n","Ejercicio 1: \n")
+print ("""
+A. Crea una tupla con los nombres de los días de la semana.
+     dias = ("L","M","X","J","V","S","D")
+     print(dias)
+B. Imprime el tercer día de la semana.
+     print(dias[2])
+C. Imprime los días de la semana en orden inverso."
+     print(dias[::-1])     \n""")
+
+# A. Crea una tupla con los nombres de los días de la semana.
+dias = ("L","M","X","J","V","S","D")
+print(dias)
+# B. Imprime el tercer día de la semana.
+print(dias[2])
+# C. Imprime los días de la semana en orden inverso.
+print(dias[::-1])
---
+++ UD.2/5.Ejs_Tuplas/Ejercicio_3.py
@@ -0,0 +1,9 @@
+"""
+        >>>>>>>    Ejercicios de Cadenas   <<<<<<
+                                                             """
+print (f">>>>>>>    Ejercicios de Cadenas   <<<<<<")                                                           
+print (f"\n","Ejercicio 2: \n")
+print ("""Los teléfonos de una empresa tienen el siguiente formato prefijo-número extensión donde el prefijo es el código del
+país +34, y la extensión tiene dos dígitos (por ejemplo +34-913724710-56). 
+Escribir un programa que pregunte por un número de teléfono con este formato en la consola y muestre por pantalla el número de 
+teléfono sin el prefijo y la extensión."\n""")
